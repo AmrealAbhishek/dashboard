c2e3611bc7451b3bede0f18f2f387fae2df719cb
Abhishek Kumar <85229113+AmrealAbhishek@users.noreply.github.com>
2023-02-05 16:15:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
-    $("#image").attr(profile.getImageUrl());
+    $("#image").attr('src',profile.getImageUrl());
     $(".data").css("display","block");
     $(".g-signin2").css("display","none");
 
